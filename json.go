dbb6957d3a0aae04dba4160e6a20a9dcfb713bb1
Lithial <jameshessell@gmail.com>
2020-07-29 11:29:06 +1200

    part 1 and 2 done. Not sure how to do part 3

--- json.go
+++ json.go
@@ -8,9 +8,12 @@ import (
 	"strconv"
 )
 
+//Companies  array of companies from json
 type Companies struct {
 	Companies []Company `json:"companies"`
 }
+
+//Company company structure for json
 type Company struct {
 	Index       int    `json:"index"`
 	CompanyName string `json:"company"`
@@ -18,21 +21,30 @@ type Company struct {
 	Address     string `json:"address"`
 	Phone       string `json:"phone"`
 	Email       string `json:"email"`
+	HasIntern   bool
 }
+
+//Interns array of Interns for json
 type Interns struct {
 	Interns []Intern `json:"interns"`
 }
+
+//Name names structure for interns in json
 type Name struct {
 	FirstName string `json:"first"`
 	LastName  string `json:"last"`
 }
+
+//Intern Intern structure for json load in
 type Intern struct {
-	InternName Name   `json:"name"`
-	Phone      string `json:"phone"`
-	Email      string `json:"email"`
-	CompanyID  int    `json:"company_id"`
+	InternName   Name   `json:"name"`
+	Phone        string `json:"phone"`
+	Email        string `json:"email"`
+	CompanyID    int    `json:"company_id"`
+	CompanyValid bool
 }
 
+//Keeping a bunch of stuff in here for now because i haven't worked out how to move it
 func main() {
 	jsonFile, err := os.Open("InternsAtCompanies2.json")
 	if err != nil {
@@ -50,24 +62,32 @@ func main() {
 	printInternData(interns, companies)
 }
 
-//TODO
+//function that does all teh heavy listing. Takes in the loaded json in the form of the interns and companies variables
 func printInternData(interns Interns, companies Companies) {
+	//used to count these things below
 	internsWithoutJobs := 0
+	companiesWithoutInterns := 0
+	//so we don't have to count these more than once
 	companyLen := len(companies.Companies)
 	internsLen := len(interns.Interns)
 
+	//loop the interns and print the relevant information.
 	for i := 0; i < internsLen; i++ {
 		var intern = interns.Interns[i]
 		var companyID = intern.CompanyID
+		fmt.Printf("Intern Name: %s\n", interns.Interns[i].InternName.FirstName)
+		fmt.Printf("Last Name: %s\n", interns.Interns[i].InternName.LastName)
 		if intern.CompanyID > 0 && intern.CompanyID < companyLen {
-			fmt.Printf("Intern Name: %s\n", interns.Interns[i].InternName.FirstName)
-			fmt.Printf("Last Name: %s\n", interns.Interns[i].InternName.LastName)
 			fmt.Printf("Company Name: %s\n", companies.Companies[companyID].CompanyName)
 			fmt.Printf("Company Email: %s\n", companies.Companies[companyID].Email)
+			//tell us this company has at least one intern
+			companies.Companies[companyID].HasIntern = true
 		}
 	}
-	fmt.Printf("Unclaimed Interns: \n")
-	for j := 0; j < companyLen; j++ {
+	//loop again to check for unclaimed interns
+	//TODO maybe add this to a struct and do the same as how we do with companies
+	fmt.Printf("\nUnclaimed Interns: \n")
+	for j := 0; j < internsLen; j++ {
 		var intern = interns.Interns[j]
 		if intern.CompanyID < 0 || intern.CompanyID > companyLen {
 			fmt.Printf("Intern Name: %s\n", interns.Interns[j].InternName.FirstName)
@@ -75,10 +95,18 @@ func printInternData(interns Interns, companies Companies) {
 			internsWithoutJobs++
 		}
 	}
+	fmt.Printf("\nCompanies without Interns: \n")
+	for k := 0; k < companyLen; k++ {
+		var company = companies.Companies[k]
+		if !company.HasIntern {
+			fmt.Printf("Company: %s\n", company.CompanyName)
+			companiesWithoutInterns++
+		}
+	}
 	fmt.Printf("***********************************************\n")
-	fmt.Printf("Number of companies %d", companyLen)
+	fmt.Printf("Companies without interns %d\n", companiesWithoutInterns)
 	fmt.Printf("***********************************************\n")
-	fmt.Printf("Interns without jobs: %d", internsWithoutJobs)
+	fmt.Printf("Interns without jobs: %d\n", internsWithoutJobs)
 
 }
 func printCompanyDetails(companies Companies) {
